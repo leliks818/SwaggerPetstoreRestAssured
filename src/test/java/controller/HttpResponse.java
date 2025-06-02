e7bce7a3cf51c249c04ac872aa8ef9e1fb4b4a99
Yuliya <yulka818818@gmail.com>
2025-06-02 14:22:16 -0400

    fix message test

--- src/test/java/controller/HttpResponse.java
+++ src/test/java/controller/HttpResponse.java
@@ -3,7 +3,6 @@ package controller;
 import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 
 import java.io.File;
 import java.util.List;
@@ -23,7 +22,7 @@ public class HttpResponse {
         this.validatableResponse = response.then();
     }
 
-    @Step("Check status code")
+    @Step("Проверка, что статус-код равен {status}")
     public HttpResponse statusCodeIs(int status) {
         validatableResponse.statusCode(status);
         return this;
@@ -33,51 +32,51 @@ public class HttpResponse {
         return validatableResponse.extract().jsonPath().getString(path);
     }
 
-    @Step("Проверка, что JSON поле '{path}' содержит подстроку '{expectedSubstring}'")
+    @Step("Проверка, что значение JSON поля '{path}' содержит подстроку '{expectedSubstring}'")
     public HttpResponse jsonValueContains(String path, String expectedSubstring) {
         String actual = validatableResponse.extract().jsonPath().getString(path);
-        assertNotNull(actual);
-        assertTrue(actual.contains(expectedSubstring));
+        assertNotNull(actual, String.format("Значение поля '%s' равно null", path));
+        assertTrue(actual.contains(expectedSubstring), String.format("Значение поля '%s' не содержит подстроку '%s'", path, expectedSubstring));
         return this;
     }
 
-    @Step("Check json value by path '{path}' and expected value '{expectedValue}'")
+    @Step("Проверка, что значение JSON поля '{path}' равно '{expectedValue}'")
     public HttpResponse jsonValueIs(String path, String expectedValue) {
         String actualValue = validatableResponse.extract().jsonPath().getString(path);
         assertEquals(expectedValue, actualValue, String.format(
-                "Actual value '%s' is not equal to expected '%s' for the path '%s'", actualValue, expectedValue, path));
+                "Фактическое значение '%s' не совпадает с ожидаемым '%s' для пути '%s'", actualValue, expectedValue, path));
         return this;
     }
 
-    @Step("Check json value by path '{path}' and expected list value")
+    @Step("Проверка, что список JSON по пути '{path}' соответствует ожидаемому")
     public <T> HttpResponse jsonValueIs(String path, List<T> expectedValue, Class<T> actualClass) {
         List<T> actualValue = validatableResponse.extract().jsonPath().getList(path, actualClass);
         assertEquals(expectedValue, actualValue, String.format(
-                "Actual value '%s' is not equal to expected '%s' for the path '%s'", actualValue, expectedValue, path));
+                "Фактический список '%s' не совпадает с ожидаемым '%s' для пути '%s'", actualValue, expectedValue, path));
         return this;
     }
 
-    @Step("Проверка, что JSON поле '{path}' не null")
+    @Step("Проверка, что JSON поле '{path}' не равно null")
     public HttpResponse jsonValueNotNull(String path) {
-        assertNotNull(validatableResponse.extract().jsonPath().get(path));
+        assertNotNull(validatableResponse.extract().jsonPath().get(path), String.format("Поле '%s' равно null", path));
         return this;
     }
 
     @Step("Проверка, что значение JSON поля '{path}' больше {number}")
     public HttpResponse jsonValueBiggerThan(String path, long number) {
         long actual = validatableResponse.extract().jsonPath().getLong(path);
-        assertTrue(actual > number);
+        assertTrue(actual > number, String.format("Ожидалось, что значение поля '%s' будет больше %d, но было %d", path, number, actual));
         return this;
     }
-    @Step("Проверка, что JSON поле '{path}' равно null")
+
+    @Step("Проверка, что значение JSON поля '{path}' равно null")
     public HttpResponse jsonValueIsNull(String path) {
         Object value = validatableResponse.extract().jsonPath().get(path);
         assertNull(value, String.format("Ожидалось, что значение поля '%s' будет null, но было '%s'", path, value));
         return this;
     }
 
-
-    @Step("Check that JSON path '{path}' is '{expectedValue}'")
+    @Step("Проверка, что значение JSON поля '{path}' равно '{expectedValue}'")
     public HttpResponse jsonValueIs(String path, Object expectedValue) {
         Object actualValue = validatableResponse.extract().jsonPath().get(path);
 
@@ -85,10 +84,10 @@ public class HttpResponse {
             double expected = ((Number) expectedValue).doubleValue();
             double actual = ((Number) actualValue).doubleValue();
             assertEquals(expected, actual, String.format(
-                    "Expected number at path '%s' to be '%s', but was '%s'", path, expected, actual));
+                    "Ожидалось, что число по пути '%s' будет '%s', но было '%s'", path, expected, actual));
         } else {
             assertEquals(expectedValue, actualValue, String.format(
-                    "Expected value at path '%s' to be '%s', but was '%s'", path, expectedValue, actualValue));
+                    "Ожидалось, что значение по пути '%s' будет '%s', но было '%s'", path, expectedValue, actualValue));
         }
 
         return this;
@@ -102,21 +101,21 @@ public class HttpResponse {
         return this;
     }
 
-    @Step("Лог тела ответа")
+    @Step("Вывод тела ответа в консоль")
     public HttpResponse logBody() {
         System.out.println(validatableResponse.extract().response().getBody().asPrettyString());
         return this;
     }
 
-    @Step("Получение списка JSON по пути: {path}")
+    @Step("Получение списка JSON объектов по пути: {path}")
     public List<Map<String, Object>> getJsonList(String path) {
         return validatableResponse.extract().jsonPath().getList(path);
     }
 
-    @Step("Проверка, что поле '{fieldName}' равно {expectedValue}")
+    @Step("Проверка, что значение поля '{fieldName}' равно {expectedValue}")
     public HttpResponse bodyFieldEquals(String fieldName, long expectedValue) {
         Number actualValue = validatableResponse.extract().jsonPath().get(fieldName);
-        assertEquals(expectedValue, actualValue.longValue());
+        assertEquals(expectedValue, actualValue.longValue(), String.format("Ожидалось, что значение поля '%s' будет %d, но было %d", fieldName, expectedValue, actualValue.longValue()));
         return this;
     }
 
